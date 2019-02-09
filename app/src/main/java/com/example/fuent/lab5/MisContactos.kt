3d36260c7d406590fd40c0fb901b2b8bcbe539bd
Marco Fuentes <marcofuentes1607@gmail.com>
2019-02-08 18:37:12 -0600

    Programa funcional

---
+++ app/src/main/java/com/example/fuent/lab5/MisContactos.kt
@@ -0,0 +1,12 @@
+package com.example.fuent.lab5
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MisContactos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mis_contactos)
+    }
+}
